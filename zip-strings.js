3bcb2c5df56cb7c2ef5c8fe5c27127533eee9364
kristinegeibel <107583931+kristinegeibel@users.noreply.github.com>
2022-07-31 22:18:34 +0200

    code not working

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,4 @@
+/* -----------code does not work---------------
 function zipStrings(strA, strB) {
   let arrA = strA.split("");
   let arrB = strB.split("");
@@ -7,8 +8,26 @@ function zipStrings(strA, strB) {
   let together = arrB.map(function (arrB, i) {
     return [arrB + arrA[i]];
   });
-
-  console.log(together);
   let backToString = together.join();
 }
-console.log(backToString);
+*/-----------------------------------------------
+
+function zipStrings(strA, strB) {
+  let arrA = strA.split("");
+  let arrB = strB.split("");
+  let zippedArray = [];
+
+  if (arrA.length >= arrB.length) {
+    for (let i = 0; i < arrA.length; i++) {
+      zippedArray.push(arrA[i]);
+      zippedArray.push(arrB[i]);
+    }
+  } else {
+    for (let i = 0; i < arrB.length; i++) {
+      zippedArray.push(arrA[i]);
+      zippedArray.push(arrB[i]);
+    }
+  }
+  let zippedString = zippedArray.join("");
+  return zippedString;
+}
